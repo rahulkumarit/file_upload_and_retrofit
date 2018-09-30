0198a29b03db83c3b65633862880eecafff933a5
rahulkumarit <rk74535@gmail.com>
2018-09-30 23:35:38 +0530

    workin on retrofit

--- app/src/main/java/com/fileupload/models/UploadImage.java
+++ app/src/main/java/com/fileupload/models/UploadImage.java
@@ -2,6 +2,32 @@ package com.fileupload.models;
 /**
  * Created by SONI on 9/30/2018.
  */
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
-public    class UploadImage {
+public class UploadImage {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- app/src/main/java/com/fileupload/retrofit/MainActivity.java
+++ app/src/main/java/com/fileupload/retrofit/MainActivity.java
@@ -1,21 +1,29 @@
 package com.fileupload.retrofit;
 
+import android.Manifest;
 import android.content.Intent;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.fileupload.EndPoints;
 import com.fileupload.R;
+import com.fileupload.models.UploadImage;
+import com.fileupload.utils.StaticUtils;
+import com.fileupload.utils.WsUtils;
+import com.fileupload.wscalling.WsFactory;
+import com.fileupload.wscalling.WsResponse;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,18 +31,15 @@ import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WsResponse {
 
     public static final int PICK_IMAGE = 100;
 
-    ApiInterface service;
+    private ApiInterface service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,14 +58,19 @@ public class MainActivity extends AppCompatActivity {
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
+                    grantPermission();
                     Intent intent = new Intent();
                     intent.setType("image/*");
                     intent.setAction(Intent.ACTION_GET_CONTENT);
                     startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE);
-                }
+
+
+                 }
             });
         }
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -70,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
             android.net.Uri selectedImage = data.getData();
             String[] filePathColumn = {MediaStore.Images.Media.DATA};
             android.database.Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+
             if (cursor == null)
                 return;
 
@@ -84,31 +95,48 @@ public class MainActivity extends AppCompatActivity {
             MultipartBody.Part body = MultipartBody.Part.createFormData("upload_file", file.getName(), reqFile);
             RequestBody name = RequestBody.create(MediaType.parse("text/plain"), "rahul");
             RequestBody paword = RequestBody.create(MediaType.parse("text/plain"), "kumar");
+
             Map<String, RequestBody> map = new HashMap<>();
 
             map.put("UserName", name);
             map.put("Password", paword);
 
+            Call loginWsCall = WsFactory.uplaodImage(body, map);
+            WsUtils.getReponse(loginWsCall, StaticUtils.REQUEST_UPLOAD, this);
 
-            retrofit2.Call<okhttp3.ResponseBody> req = service.postImage(body, map);
-
-            req.enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    try {
-                        Toast.makeText(MainActivity.this, "response:" + response.body().string(), Toast.LENGTH_SHORT).show();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    t.printStackTrace();
-                    Toast.makeText(MainActivity.this, "failure response", Toast.LENGTH_SHORT).show();
-
-                }
-            });
         }
     }
+
+    @Override
+    public void successResponse(Object response, int code) {
+        switch (code) {
+            case StaticUtils.REQUEST_LOGIN:
+                break;
+            case StaticUtils.REQUEST_UPLOAD:
+                UploadImage uploadImage = (UploadImage) response;
+                Toast.makeText(MainActivity.this, "File Upload Response:" + uploadImage.getStatus(), Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    @Override
+    public void failureRespons(Throwable error, int code) {
+
+    }
+
+    private void grantPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            finish();
+            startActivity(intent);
+            return;
+        }
+    }
+
 }
